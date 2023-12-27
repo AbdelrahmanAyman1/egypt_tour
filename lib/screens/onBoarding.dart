c365bc975f7a46647287ad24aefdf81efe16c00b
boody <boodayman936@gmail.com>
2023-12-27 23:16:13 +0200

    create page view

--- lib/screens/onBoarding.dart
+++ lib/screens/onBoarding.dart
@@ -1,5 +1,7 @@
+import 'package:egypt_tour/constant/constant.dart';
 import 'package:egypt_tour/model/onboarding_model.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OnBoarding extends StatefulWidget {
   static String routeName = 'onBoarding';
@@ -20,6 +22,43 @@ class _OnBoardingState extends State<OnBoarding> {
         leading: Image.asset(
           'assets/images/img.png',
         ),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            child: PageView.builder(
+              onPageChanged: (value) {
+                currentIndex = value;
+                setState(() {});
+              },
+              itemCount: content.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Image.asset(content[currentIndex].image),
+                    Text(
+                      content[currentIndex].title,
+                      style: GoogleFonts.radioCanada(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xff89C9FF)),
+                    ),
+                    Center(
+                      child: Text(
+                          textAlign: TextAlign.center,
+                          content[currentIndex].desc,
+                          style: GoogleFonts.radioCanada(
+                            fontSize: 18,
+                            color: Colors.black,
+                          )),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
