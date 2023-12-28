5792f5ad478aac1bec88e84a3403a37d018777c7
boody <boodayman936@gmail.com>
2023-12-28 21:31:27 +0200

    add title of page

--- lib/screens/sing_up.dart
+++ lib/screens/sing_up.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SingUp extends StatelessWidget {
   static String routeName = 'singUp';
@@ -12,11 +13,22 @@ class SingUp extends StatelessWidget {
       body: Column(
         children: [
           Center(
-              child: Image.asset(
-            'assets/images/logo.png',
-            height: 154.h,
-            width: 153.w,
-          ))
+            child: Image.asset(
+              'assets/images/logo.png',
+              height: 154.h,
+              width: 153.w,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            'Sing up',
+            style: GoogleFonts.radioCanada(
+                fontWeight: FontWeight.bold, fontSize: 25.sp),
+          ),
+          Text(
+            'please sign in to continue',
+            style: GoogleFonts.radioCanada(fontSize: 15.sp, color: Colors.grey),
+          ),
         ],
       ),
     );
