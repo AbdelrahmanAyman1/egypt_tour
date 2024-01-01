ed29942455eb94ab01f63e10aca536d1a3828a7b
boody <boodayman936@gmail.com>
2024-01-02 00:45:32 +0200

    validate if text field is empty

--- lib/widgets/forget_password_form.dart
+++ lib/widgets/forget_password_form.dart
@@ -5,33 +5,53 @@ import 'package:google_fonts/google_fonts.dart';
 import 'custom_button.dart';
 import 'custom_text_feild.dart';
 
-class ForgetPasswordForm extends StatelessWidget {
+class ForgetPasswordForm extends StatefulWidget {
   const ForgetPasswordForm({super.key});
 
   @override
+  State<ForgetPasswordForm> createState() => _ForgetPasswordFormState();
+}
+
+class _ForgetPasswordFormState extends State<ForgetPasswordForm> {
+  TextEditingController emailController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autoValidateMode = AutovalidateMode.disabled;
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(
-          textAlign: TextAlign.center,
-          'That\'s ok. it happens! Enter your email to reset password ',
-          style: GoogleFonts.radioCanada(fontSize: 18.sp),
-        ),
-        SizedBox(height: 10.h),
-        const CustomTextField(
-            hint: 'Enter your email',
-            keyboardType: TextInputType.emailAddress,
-            errorMessage: 'please enter your email'),
-        SizedBox(height: 10.h),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 100.w),
-          child: CustomButton(
-            text: 'Reset Password',
-            onPressed: () {},
+    return Form(
+      key: formKey,
+      autovalidateMode: autoValidateMode,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            textAlign: TextAlign.center,
+            'That\'s ok. it happens! Enter your email to reset password ',
+            style: GoogleFonts.radioCanada(fontSize: 18.sp),
           ),
-        )
-      ],
+          SizedBox(height: 10.h),
+          const CustomTextField(
+              hint: 'Enter your email',
+              keyboardType: TextInputType.emailAddress,
+              errorMessage: 'please enter your email'),
+          SizedBox(height: 10.h),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 100.w),
+            child: CustomButton(
+              text: 'Reset Password',
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  formKey.currentState!.save();
+                } else {
+                  autoValidateMode = AutovalidateMode.always;
+                  setState(() {});
+                }
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
