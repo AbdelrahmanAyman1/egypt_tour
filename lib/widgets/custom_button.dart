15c3ae886a0f94b453f267b90a5fcac19b72e7a8
boody <boodayman936@gmail.com>
2024-01-01 23:36:47 +0200

    auth with firebase singIn & singUP and singIn with google

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -4,18 +4,19 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text});
+  const CustomButton({super.key, required this.text, this.onPressed});
 
   final String text;
+  final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: primaryColor,
-          foregroundColor: Colors.white,
-          padding: EdgeInsetsDirectional.symmetric(vertical: 10.h)),
-        onPressed: () {},
+            backgroundColor: primaryColor,
+            foregroundColor: Colors.white,
+            padding: EdgeInsetsDirectional.symmetric(vertical: 10.h)),
+        onPressed: onPressed,
         child: Text(
           text,
           style: GoogleFonts.radioCanada(color: Colors.white, fontSize: 16.sp),
