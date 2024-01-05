f19a4c9c6bb40e2e85fa602642c4fc14f71a484b
boody <boodayman936@gmail.com>
2024-01-05 23:27:48 +0200

    make tabs

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,19 +1,45 @@
 import 'package:egypt_tour/constant/constant.dart';
 import 'package:egypt_tour/screens/sing_in.dart';
+import 'package:egypt_tour/screens/tabs/home_tab.dart';
+import 'package:egypt_tour/screens/tabs/location_tab.dart';
+import 'package:egypt_tour/screens/tabs/profile_tab.dart';
+import 'package:egypt_tour/screens/tabs/search_tab.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../widgets/custom_nav_bar.dart';
+
+class HomeScreen extends StatefulWidget {
   static String routeName = 'home';
 
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  List<Widget> tabs = const [
+    HomeTab(),
+    SearchTab(),
+    LocationTab(),
+    ProfileTab()
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: const CustomBottomNavBar(),
+      bottomNavigationBar: CustomBottomNavBar(
+        index: index,
+        onTabChange: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+      ),
       appBar: AppBar(
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
@@ -28,65 +54,16 @@ class HomeScreen extends StatelessWidget {
               icon: const Icon(Icons.logout))
         ],
         centerTitle: true,
-        title: const Text('home'),
+        title: Text(index == 0
+            ? 'Home'
+            : index == 1
+                ? 'Search'
+                : index == 2
+                    ? 'Location'
+                    : 'Profile'),
         backgroundColor: primaryColor,
       ),
+      body: tabs[index],
     );
-  }
-}
-
-class CustomBottomNavBar extends StatefulWidget {
-  const CustomBottomNavBar({
-    super.key,
-  });
-
-  @override
-  State<CustomBottomNavBar> createState() => _CustomBottomNavBarState();
-}
-
-class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
-  @override
-  Widget build(BuildContext context) {
-    return GNav(
-        tabMargin:
-            EdgeInsetsDirectional.symmetric(horizontal: 10.w, vertical: 10.h),
-        rippleColor: Colors.white,
-        // tab button ripple color when pressed
-        hoverColor: Colors.white,
-        // tab button hover color
-
-        tabBorderRadius: 15,
-        curve: Curves.linear,
-        // tab animation curves
-        gap: 8,
-        // the tab button gap between icon and text
-        color: Colors.black,
-        // unselected icon color
-        activeColor: Colors.white,
-        // selected icon and text color
-        iconSize: 24,
-        // tab button icon size
-        tabBackgroundColor: primaryColor,
-        // selected tab background color
-        padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 4.h),
-        // navigation bar padding
-        tabs: const [
-          GButton(
-            icon: Icons.home,
-            text: 'Home',
-          ),
-          GButton(
-            icon: Icons.search,
-            text: 'Search',
-          ),
-          GButton(
-            icon: Icons.directions_car_rounded,
-            text: 'Location',
-          ),
-          GButton(
-            icon: Icons.person,
-            text: 'Profile',
-          )
-        ]);
   }
 }
